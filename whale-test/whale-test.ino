23e4b7998ac79684b9abcf87919b5b6e8e04221c
Reed Morse <reed.morse@gmail.com>
2019-02-22 15:51:38 -0800

    first basic anim works

--- whale-test/whale-test.ino
+++ whale-test/whale-test.ino
@@ -1,6 +1,6 @@
 #include "FastLED.h"
 
-#define DATA_PIN 1
+#define DATA_PIN 3
 #define LED_TYPE WS2811
 #define COLOR_ORDER GRB
 #define NUM_LEDS 50
@@ -9,10 +9,9 @@
 
 #define NUM_PIXELS 12
 CRGB _baseColor[NUM_PIXELS] = { 0 };
+int _basePointer = -1;
 CRGB _pixelBuffer[NUM_PIXELS] = { 0 };
-
-CRGB _leds[NUM_LEDS];
-unsigned long _lastCrawl = 0;
+CRGB _leds[NUM_LEDS] = { 0 };
 
 DEFINE_GRADIENT_PALETTE(_whaleColors) {
 //  0, 0, 48, 170,
@@ -27,40 +26,58 @@ void setup() {
   Serial.begin(9600);
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(_leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
-  fill_solid(_leds, NUM_LEDS, CRGB::Black);
+//  fill_solid(_leds, NUM_LEDS, CRGB::Black);
 }
 
 void loop() {
-  FastLED.show();  
-  FastLED.delay(1000/FRAMES_PER_SECOND);
+  FastLED.show();
+  FastLED.delay(1000 / FRAMES_PER_SECOND);
 
   unsigned long now = millis();
 
   drawBaseColor(now);
   
 
-//  for (int i = NUM_LEDS - 1; i > 0; i--) {
-//    _leds[i] =  _leds[i - 1];
-//  }
+  for (int i = 0; i < NUM_PIXELS; i++) {
+    _pixelBuffer[i] = _baseColor[i];
+  }
 
-  _pixelBuffer[0] = ColorFromPalette((CRGBPalette16)_whaleColors, random8(), random8(50, 255), LINEARBLEND);
+  render();
 }
 
 void drawBaseColor(unsigned long nowMS) {
-  const unsigned long durationMS = 5000;
+  const float durationMS = 3000;
   double fractionComplete = calculateFractionComplete(nowMS, durationMS);
-  
+
+  // Use integer to automatically round down.
   int next = fractionComplete * NUM_PIXELS;
-  Serial.println(next);
+  // Subtract the whole value from the whole + frational value 
+  // to get "% done with this pixel, until we switch to next pixel"
+  float nextFraction = fractionComplete * NUM_PIXELS - next;
 
-  
-  _base[next] = ColorFromPalette((CRGBPalette16)_whaleColors, random8(), random8(50, 255), LINEARBLEND);
+  // Fade the other pixels
+  for (int i = 0; i < NUM_PIXELS; i++ ) {
+    if (i == next) { continue; }  
+    _baseColor[i].fadeToBlackBy(3);
+  }
 
-//  _baseColor
+  if (next != _basePointer) {
+    _basePointer = next;
+    // Use 1 for brightness, we will fade it in.
+    _baseColor[next] = ColorFromPalette((CRGBPalette16)_whaleColors, random8(), 1, LINEARBLEND);
+  }
+
+  // Step brightness to max, then down to the % we actually want.
+  _baseColor[next].maximizeBrightness(255);
+  _baseColor[next].maximizeBrightness(nextFraction * 255);
+
+  Serial.print(nextFraction);
+  Serial.print("\t");
+  Serial.println(255 * nextFraction);
 }
 
-float calculateFractionComplete(unsigned long nowMS, unsigned long durationMS) {
-  return (nowMS % durationMS) / ((float)durationMS);
+float calculateFractionComplete(unsigned long nowMS, float durationMS) {
+  return (nowMS % (unsigned long)durationMS) / durationMS;
 }
 
 // Render out the pixel buffer to the physical LEDs. This emulates 3 strands using one single LED strand.
